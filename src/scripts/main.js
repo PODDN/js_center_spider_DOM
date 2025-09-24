3c60b31272693cc05d6ba5f92782f18035b62e3d
Denys Piddubnyi <iforgotmypincode@gmail.com>
2025-09-24 22:01:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,28 @@
 'use strict';
 
-// write code here
-const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallWidth = wall.clientWidth;
-const wallHeight = wall.clientHeight;
+const centerSpider = () => {
+  const wall = document.querySelector('.wall');
 
-const spiderWidth = spider.clientWidth;
-const spiderHeight = spider.clientHeight;
+  if (!wall || !spider) return;
 
-spider.style.top = wallHeight / 2 - spiderHeight / 2 + 'px';
-spider.style.left = wallWidth / 2 - spiderWidth / 2 + 'px';
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  spider.style.position = 'absolute';
+  wall.style.position = 'relative';
+
+  spider.style.top = wallHeight / 2 - spiderHeight / 2 + 'px';
+  spider.style.left = wallWidth / 2 - spiderWidth / 2 + 'px';
+};
+
+if (spider.complete) {
+  centerSpider();
+} else {
+  spider.addEventListener('load', centerSpider);
+}
+
