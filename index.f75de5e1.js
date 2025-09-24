43af70f144401d37b05772534acf1d6e56c3df38
Denys Piddubnyi <iforgotmypincode@gmail.com>
2025-09-24 22:01:45 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const centerSpider = ()=>{
+    const wall = document.querySelector(".wall");
+    if (!wall || !spider) return;
+    const wallWidth = wall.clientWidth;
+    const wallHeight = wall.clientHeight;
+    const spiderWidth = spider.clientWidth;
+    const spiderHeight = spider.clientHeight;
+    spider.style.position = "absolute";
+    wall.style.position = "relative";
+    spider.style.top = wallHeight / 2 - spiderHeight / 2 + "px";
+    spider.style.left = wallWidth / 2 - spiderWidth / 2 + "px";
+};
+if (spider.complete) centerSpider();
+else spider.addEventListener("load", centerSpider);
+
+//# sourceMappingURL=index.f75de5e1.js.map
